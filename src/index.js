33de220d622496bc93fc3d1123c057095b5b2d0e
maryann <amaraizummesoma@gmail.com>
2022-06-17 00:00:20 +0100

    my Javascript solution

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 // complete the function
 function prime (num) {
-  // code goes here
-}
-
-function solution (arg) {
-  return prime(arg);
-}
-
-if (typeof require !== "undefined" && require.main === module) {
-  if (process.argv.length <= 2) {
-    throw Error("Require argument");
+    // code goes 
+      num=parseInt(prompt("Enter a number:"))
+    
+  
+    let isPrime=true;
+    if(num<=1){
+      console.log("It is not a prime number")
+    }
+    for (let i=2;i<num;i++){
+      if(num%i==0){
+       return isPrime=false;
+      }
+     return isPrime=true;
+  
+    }
   }
-
-  const arg = process.argv[2];
-  console.log(solution(arg));
-}
-
-module.exports = { solution };
+  prime()
+  //END OF CODE
+  
+  function solution (arg) {
+    return prime(arg);
+  }
+  
+  if (typeof require !== "undefined" && require.main === module) {
+    if (process.argv.length <= 2) {
+      throw Error("Require argument");
+    }
+  
+    const arg = process.argv[2];
+    console.log(solution(arg));
+  }
+  
+  module.exports = { solution };
+  
+  
+  
